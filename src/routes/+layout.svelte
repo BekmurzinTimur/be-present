e22e92374ab46d97db148033a5a0bf13e06e076f
Timur Bekmurzin <bekmurzint@yandex.ru>
2025-12-28 13:53:30 +0500

    Added meta description and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,9 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={'/assets/icon48.png'} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={'/assets/icon48.png'} />
+	<title>Be Present</title>
+	<meta name="description" content="Gentle reminders to stay present" />
+</svelte:head>
 {@render children()}
